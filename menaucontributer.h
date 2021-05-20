05a14d7138a503a923e4c69a0637898bfd8dfb42
florusdernomus <43517433+florusdernomus@users.noreply.github.com>
2021-05-20 11:25:43 +0200

    me nau contibutt
    me nau contibutt

---
+++ menaucontributer.h
@@ -0,0 +1,14 @@
+#ifndef _me_contrubute_
+#define _me_contribute_
+
+class contributer
+{
+private:
+    bool iscontributer;
+public:
+    void yes()
+    {
+        iscontributer = true;
+    };
+};
+#endif
